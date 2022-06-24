281f369af25cbbf07863b9be55d24fb355fdb9ec
HARINI2806 <ganeshanharini28@gmail.com>
2022-06-24 20:47:21 +0530

    3D CNN Code for ASD_Detection added

---
+++ 3D CNN/cnn_3d.py
@@ -0,0 +1,179 @@
+import tensorflow as tf
+import numpy as np
+import h5py
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from nn_utils import conv3d, conv3d_transpose
+
+import sys
+import time
+
+slim = tf.contrib.slim
+
+DATA_DIR = 'data'
+
+class Config():
+
+    lr = 0.001
+    l2 = 0.01
+    dropout = 0.8
+
+    kernel_size = 3 
+    num_filters = 8
+    downsample_every = 2
+
+    num_layers = 4
+    num_layers_to_train = num_layers
+    num_layers_to_restore = 0
+    downsample_every = 2
+    downsample_factor = 2
+    use_sex_labels = False
+
+    mode = 'pretrain'
+
+
+class CNN_3D(object):
+
+    def _save_images(self, images, outputs, name):
+        if len(outputs.shape) == 4:
+            outputs = np.expand_dims(outputs, 4)
+        im_depth = images.shape[1]/2
+        out_depth = outputs.shape[1]/2
+        fig = plt.figure()
+        a = fig.add_subplot(1,2,1)
+        imgplot = plt.imshow(images[0,im_depth,:,:])
+        a.set_title('Original')
+        a = fig.add_subplot(1,2,2)
+        imgplot = plt.imshow(outputs[0,out_depth,:,:,0])
+        a.set_title('Reconstructed')
+        fig.savefig('figures/' + name + ".png")
+        plt.close()
+
+    def make_predictions(self, output):
+        """Get answer predictions from output"""
+        preds = tf.nn.softmax(output)
+        pred = tf.argmax(preds, 1)
+        return pred
+
+    def calc_accuracy(self, predictions, labels):
+        correct_prediction = tf.equal(predictions, labels)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        return accuracy
+
+    def calc_loss(self, outputs, labels):
+
+        # in pretraining outputs and labels are the full 3d MRIs
+        if self.config.mode == 'pretrain':
+            loss = tf.reduce_sum(tf.square(outputs - labels))
+        else:
+            loss = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(outputs, labels)) 
+        loss += tf.reduce_sum(tf.pack(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)))
+
+
+        return loss
+
+    def add_train_op(self, loss):
+        train_op = tf.train.AdamOptimizer(learning_rate=self.config.lr).minimize(loss)
+        return train_op
+
+    def correlation_inference(self, correlation):
+        flattened = slim.flatten(correlation)
+        layer_1 = slim.fully_connected(flattened, 1000, weights_regularizer=slim.l2_regularizer(self.config.l2))
+        output = slim.fully_connected(layer_1, 2, activation_fn=None, weights_regularizer=slim.l2_regularizer(self.config.l2))
+        #output = slim.fully_connected(flattened, 2, activation_fn=None, weights_regularizer=slim.l2_regularizer(self.config.l2))
+        return output
+
+    def image_inference(self, images, train=True):
+
+        images = tf.expand_dims(images, -1)
+
+        # for convenience
+        num_filters = self.config.num_filters
+        kernel_size = self.config.kernel_size
+        downsample_every = self.config.downsample_every
+        num_layers = self.config.num_layers
+        num_layers_to_train = self.config.num_layers_to_train
+
+        # conv
+        if num_layers > 0:
+            trainable = True if num_layers_to_train >= num_layers else False
+            forward = conv3d(images, kernel_size, 1, num_filters,
+                    scope='conv_1', trainable=trainable)
+        else:
+            forward = images
+
+        for i in range(num_layers - 1):
+            stride = 2 if i % downsample_every == 0 else 1
+            trainable = True if i+2 > num_layers - num_layers_to_train else False
+            forward = conv3d(forward, kernel_size, num_filters, num_filters,
+                    scope='conv_' + str(i+2), stride=stride, trainable=trainable)
+            if stride == 2:
+                forward = slim.dropout(forward, keep_prob=self.config.dropout, is_training=train)
+
+        if self.config.mode == 'pretrain':
+            backward = forward
+            # deconv
+            for i in range(num_layers - 1):
+                stride = 1 if i % downsample_every == 0 else 2
+                backward = conv3d_transpose(backward, kernel_size, num_filters, num_filters,
+                        scope='conv_' + str(num_layers - i), stride=stride)
+            backward = conv3d_transpose(backward, kernel_size, num_filters, 1, scope='conv_1', stride=2)
+            output = tf.squeeze(backward)
+        else:
+
+            flattened = slim.flatten(forward)
+        
+            with tf.variable_scope('fully_connected'):
+                # fully connected layer
+                output = slim.fully_connected(flattened, 2000, weights_regularizer=slim.l2_regularizer(self.config.l2))
+                output = slim.fully_connected(output, 500, weights_regularizer=slim.l2_regularizer(self.config.l2))
+                output = slim.fully_connected(output, 2, activation_fn=None, weights_regularizer=slim.l2_regularizer(self.config.l2))
+
+        return output, forward
+
+    def inference(self, images, correlation, train):
+        if self.config.use_correlation == 2: 
+            corr_outputs = self.correlation_inference(correlation)
+            return corr_outputs
+        image_outputs, self.filt = self.image_inference(images, train)
+
+        # pool correlation and image results
+        if self.config.use_correlation == 1:
+            corr_outputs = self.correlation_inference(correlation)
+            outputs = tf.concat(1, [image_outputs, corr_outputs])
+            outputs = slim.fully_connected(outputs, 2, activation_fn=None)
+            return outputs
+
+        return image_outputs
+
+    def add_summaries(self, images, train):
+        dataset = 'train' if train else 'validation'
+        tf.scalar_summary('loss', self.loss)
+        tf.scalar_summary('accuracy', self.accuracy)
+        if self.config.use_correlation != 2:
+            filt_depth = int(self.filt.get_shape()[1])/2
+            im_depth = int(images.get_shape()[1])/2
+            filt = tf.squeeze(
+                    tf.slice(self.filt, [0, filt_depth, 0, 0, 0], [-1, 1, -1, -1, 1])
+                    , [1])
+            image = tf.squeeze(
+                    tf.slice(images, [0, im_depth, 0, 0], [-1, 1, -1, -1])
+                    )
+            tf.image_summary('filter', filt)
+            tf.image_summary('image', tf.expand_dims(image, -1))
+
+    def __init__(self, config, image_batch, label_batch, corr_batch, train=True):
+        self.config = config
+        label_batch = image_batch if config.mode == 'pretrain' else label_batch
+        self.outputs = self.inference(image_batch, corr_batch, train)
+        self.loss = self.calc_loss(self.outputs, label_batch)
+        self.predictions = self.make_predictions(self.outputs)
+        self.accuracy = self.calc_accuracy(self.predictions, label_batch)
+        self.train_op = self.add_train_op(self.loss)
+        self.add_summaries(image_batch, train)
+        self.merged = tf.merge_all_summaries()
+        
+
---
+++ 3D CNN/train_cnn.py
@@ -0,0 +1,180 @@
+import tensorflow as tf
+import numpy as np
+import argparse
+import time
+import os
+
+from cnn_3d import CNN_3D, Config
+import mri_input
+from test_cnn import test_cnn
+import nn_utils
+
+import sys
+
+BATCH_SIZE = 15
+MAX_STEPS = 10000
+SAVE_EVERY = 10
+MIN_EXAMPLES_IN_QUEUE = 1000
+EARLY_STOPPING = 50
+
+def train_cnn(config):
+
+    mode = config.mode
+
+    save_path = nn_utils.get_save_path(config)
+
+    intro_str = '==> Building 3D CNN with %d layers'
+    print intro_str % (config.num_layers)
+    if config.use_sex_labels:
+        print 'Debugging by training on gender labels'
+
+    fn = 'data/mri_{}train.tfrecords'.format(config.gate)
+    print '==> Reading examples from', fn
+    filename_queue = tf.train.string_input_producer([fn], num_epochs=None)
+
+    with tf.device('/cpu:0'):
+        image, label, sex, corr = mri_input.read_and_decode_single_example(filename_queue,
+                downsample_factor=config.downsample_factor, corr=config.use_correlation,
+                rotate=config.rotate, noise=config.noise)
+
+        image_batch, label_batch, sex_batch, corr_batch = tf.train.shuffle_batch(
+            [image, label, sex, corr], batch_size=BATCH_SIZE,
+            capacity=10000,
+            min_after_dequeue=MIN_EXAMPLES_IN_QUEUE
+            )
+
+    label_batch = sex_batch if config.use_sex_labels else label_batch 
+
+    cnn = CNN_3D(
+            config,
+            image_batch,
+            label_batch,
+            corr_batch,
+            )
+
+    # only restore layers that were previously trained
+    pretrained_names = ['conv_' + str(i+1) + '/weights:0' for i in range(config.num_layers_to_restore)]
+    pretrained_vars = [v for v in tf.all_variables() if v.name in pretrained_names]
+
+    print '==> variables to be restored:'
+    for v in pretrained_vars:
+        print v.name
+    print '==> variables to be trained:'
+    for v in tf.trainable_variables():
+        print v.name
+
+
+    sess = tf.Session()
+
+    summary_writer = tf.train.SummaryWriter('summaries/' + config.sum_dir + '/{}/train'.format(save_path[8:]), sess.graph)
+
+    init = tf.initialize_all_variables()
+    sess.run(init)
+
+
+    if config.num_layers_to_restore > 0:
+        restorer = tf.train.Saver(pretrained_vars)
+        print '==> restoring weights'
+        #path = 'weights/cae_pretrain_{}.weights'.format(str(num_layers_to_restore))
+        path = 'weights/greedy_6layer_weights.weights'
+        assert os.path.exists(path)
+        restorer.restore(sess, path)
+    if config.num_layers_to_restore == -1:
+        restorer = tf.train.Saver()
+        path = 'weights/cae_supervised.weights'
+        assert os.path.exists(path)
+        restorer.restore(sess, path)
+
+    saver = tf.train.Saver()
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    train_accuracy = 0
+    best_val_loss = None
+
+    for step in xrange(MAX_STEPS):
+        start_time = time.time()
+        _, loss_value, image_value, output_value, accuracy, summary = sess.run([
+                    cnn.train_op,
+                    cnn.loss,
+                    image_batch,
+                    cnn.outputs,
+                    cnn.accuracy,
+                    cnn.merged])
+        #print output_value
+        train_accuracy += accuracy
+        duration = time.time() - start_time
+        summary_writer.add_summary(summary, step)
+
+        if step % 2 == 0:
+            num_examples_per_step = BATCH_SIZE
+            sec_per_batch = float(duration)
+
+            format_str = ('step %s, loss = %.2f (%.3f '
+                          'sec/batch)')
+            s = format_str % (step, loss_value, sec_per_batch)
+            sys.stdout.write('\r' + s + ' ')
+            sys.stdout.flush()
+
+        if (step % SAVE_EVERY == 0 or (step + 1) == MAX_STEPS) and step != 0:
+            if mode == 'supervised':
+                saver.save(sess, save_path)
+                print '==> evaluating valid and train accuracy'
+                val_accuracy, val_loss = test_cnn(config, start_step=step)
+
+                print 'train accuracy:', train_accuracy/float(SAVE_EVERY) 
+                print 'val accuracy:', val_accuracy
+                train_accuracy = 0
+
+                if val_loss < best_val_loss or best_val_loss is None:
+                    early_stopping_count = 0
+                    best_val_loss = val_loss
+                    print '==> saving weights to', save_path + '_best'
+                    saver.save(sess, save_path + '_best')
+                else:
+                    early_stopping_count += 1
+                    if early_stopping_count >= EARLY_STOPPING: break
+
+                #cnn._save_images(image_value, filter_val, 'new_' + str(step))
+            else:
+                saver.save(sess, 
+                        'weights/cae_' + mode + '_' + str(num_layers) + '.weights')
+                #cnn._save_images(image_value, output_value, 'new_' + str(step))
+    coord.request_stop()
+    coord.join(threads)
+    sess.close()
+    tf.reset_default_graph()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--mode", default="supervised")
+    parser.add_argument("-l", "--num-layers", type=int, default=4)
+    parser.add_argument("-r", "--num-layers-to-restore", type=int, default=0)
+    parser.add_argument("-t", "--num-layers-to-train", type=int, default=4, 
+            help="trains the specified number of innermost layers")
+    parser.add_argument("-d", "--downsample_factor", type=int, default=2)
+    parser.add_argument("-s", "--use_sex_labels", type=bool, default=False)
+    parser.add_argument("-c", "--use_correlation", type=int, default=0, help="0 indicates no use, 1 supplements, 2 trains on only correlation")
+    parser.add_argument('-g', '--gate', default='')
+    parser.add_argument('-sd', '--sum-dir', default='')
+    parser.add_argument('-ro', '--rotate', type=bool, default=True)
+    parser.add_argument('-no', '--noise', type=float, default=0.1)
+    args = parser.parse_args()
+    config = Config()
+    config.gate = args.gate
+    config.num_layers = args.num_layers
+    config.num_layers_to_train = args.num_layers_to_train
+    config.mode = args.mode
+    config.num_layers_to_restore = args.num_layers_to_restore
+    config.downsample_factor = args.downsample_factor
+    config.use_sex_labels = args.use_sex_labels
+    config.use_correlation = args.use_correlation
+    config.sum_dir = args.sum_dir
+    config.rotate = args.rotate
+    config.noise = args.noise
+    train_cnn(config)
+
+       
